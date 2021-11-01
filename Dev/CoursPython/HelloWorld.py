34e121a1c76d6b7f149fa0c294ac5825a078c425
bernie350 <92715832+bernie350@users.noreply.github.com>
2021-11-01 16:54:18 +0100

    Create HelloWorld.py

---
+++ Dev/CoursPython/HelloWorld.py
@@ -0,0 +1,16 @@
+# Code issu du cours de OpenClassroom
+x = 3
+y = 5
+print ("J'apprends Python !")
+livre = "Zemmour, la Gargamel de 2021"
+print (livre)
+livre = "Je préfère Marine Le Pen"
+print (livre)
+nom = "Julien"
+print(nom)
+z = x + y
+print(z)
+Bool = True
+print(Bool)
+plateformes_sociales = ["Facebook", "Instagram", "Snapchat", "Twitter", "OnlyFans"]
+
