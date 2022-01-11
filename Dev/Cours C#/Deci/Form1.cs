1c58e049ff20fad136937648093ea71a226e16e5
bernie350 <92715832+bernie350@users.noreply.github.com>
2022-01-11 10:55:52 +0100

    Comm

--- Dev/Cours C#/Deci/Form1.cs
+++ Dev/Cours C#/Deci/Form1.cs
@@ -9,38 +9,38 @@ namespace Deci
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string nombreATester = textBox1.Text;
-            if (int.TryParse(nombreATester, out int value))
+            string nombreATester = textBox1.Text; // Récupération de la valeur saisie
+            if (int.TryParse(nombreATester, out int value)) // Vérification de la valeur
             {
                 int nbPremier = Int32.Parse(nombreATester);
-                if (nbPremier <= 1)
+                if (nbPremier <= 1) // S'il est premier
                 {
-                    MessageBox.Show("Le nombre saisi n'est pas un nombre premier !");
+                    MessageBox.Show("Le nombre saisi n'est pas un nombre premier !"); // Affichage d'une erreur de saisie
                 }
                 else
                 {
-                    var diviseurs = new List<int>();
+                    var diviseurs = new List<int>(); // Création de l'Array qui va stocker les diviseurs
                     for (var i = 1; i < nbPremier; i++)
                     {
-                        if (nbPremier % i == 0)
+                        if (nbPremier % i == 0) // Vérification par modulo
                         {
-                            diviseurs.Add(i);
+                            diviseurs.Add(i); // Ajout dans l'Array des diviseurs
                         }
                     }
-                    if (diviseurs.Count <= 2)
+                    if (diviseurs.Count <= 2) // Si les diviseurs sont uniquement 1, alors le nombre est primaire
                     {
-                        radioButton1.Checked = true;
+                        radioButton1.Checked = true; // Changement de l'état du bouton de radio
                     }
                     else
                     {
-                        label3.Text = string.Join(",", diviseurs.ToArray());
-                        radioButton2.Checked = true;
+                        label3.Text = string.Join(",", diviseurs.ToArray()); // Affichage des diviseurs sur le label correspondant
+                        radioButton2.Checked = true; // Changement de l'état du bouton radio
                     }
                 }
             }
             else
             {
-                MessageBox.Show("Le nombre saisi n'est pas valide !");
+                MessageBox.Show("Le nombre saisi n'est pas valide !"); // Affichage d'une erreur de saisie
             }
         }
 
